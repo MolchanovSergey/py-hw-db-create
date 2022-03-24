3443a58f898985af56699f77fb24ff1a872a5b14
Молчанов Сергей <molchanov.sergey@hotmail.com>
2022-03-24 18:05:40 +0300

    Изменение сязи между альбомами и исполнителями

--- py49.sql
+++ py49.sql
@@ -1,17 +1,16 @@
-
 create table if not exists musician(
 id serial primary key,
 musician_name varchar(100) not null,
 musician_nikname varchar(100) not null unique
 );
 
-create table if not exists collection (
+create table if not exists collection(
 id serial primary key,
 collection_name varchar (100) not null unique,
 collection_release_year date not null
 );
 
-create table if not exists genre (
+create table if not exists genre(
 id serial primary key,
 genre_name varchar (100) not null unique
 );
@@ -22,13 +21,17 @@ musician_id integer references musician(id),
 constraint pk primary key (genre_id, musician_id)
 );
 
-create table if not exists album (
+create table if not exists album(
 id serial primary key,
 album_name varchar(100) not null,
-album_release_year date not null,
-musician_id integer references musician(id)
+album_release_year date not null
 );
 
+create table if not exists AlbumMusician(
+album_id integer references album(id),
+musician_id integer references musician(id),
+constraint pk_album_musician primary key (album_id, musician_id)
+);
 
 create table if not exists track(
 id serial primary key,
@@ -37,7 +40,7 @@ duration_track interval minute to second not null,
 album_id integer references album(id)
 );
 
-create table if not exists CollectionTrack (
+create table if not exists CollectionTrack(
 collection_id integer references collection(id),
 track_id integer references track(id),
 constraint pk_collection_track primary key (collection_id, track_id)
@@ -45,3 +48,6 @@ constraint pk_collection_track primary key (collection_id, track_id)
 
 
 
+
+
+  
